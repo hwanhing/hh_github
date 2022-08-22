f217d2085f1db77b9792e0f710d2fced7c056a4a
환희 <111285100+hwanhing@users.noreply.github.com>
2022-08-22 14:58:33 +0900

    Update Alian_info.java

--- src/main/Alian_info.java
+++ src/main/Alian_info.java
@@ -20,9 +20,6 @@ public class Alian_info extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
